9226654ec9e7d80cd1e90a71334b74215012c358
ttibsi <ashisbitt@icloud.com>
2025-02-01 17:38:54 +0000

    setup and rewrite datum

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,51 @@
 pub mod datum;
+
+use crate::datum::Datum;
+use std::collections::HashMap;
+
+// ADD K V (set k's value to v. If k has a list as it's value, append it)
+// ADD K (set v to True)
+// ADD K LIST (create an empty list as the value)
+// SHOW (print the whole store)
+// SHOW K (Show the value(s) of k)
+// DEL K (Delete k and it's values)
+// DEL K V (If K is a list, delete the value v from it's list)
+//
+
+pub fn parse_input(store: &mut HashMap<String, Datum>, input: String) -> Option<String> {
+    let cmds: Vec<&str> = input.split("|").collect();
+    for cmd in cmds {
+        let parts: Vec<&str> = cmd.split(" ").collect();
+
+        if parts[0] == "ADD" {
+        } else if parts[0] == "SHOW" {
+            let mut ret = String::new();
+
+            store.into_iter().for_each(|(key, value)| {
+                ret += key;
+                ret += ": ";
+                ret += &value.to_string().clone();
+                ret += "\n";
+            });
+
+            return Some(ret);
+        } else if parts[0] == "DEL" {
+        } else {
+            return Some(String::from("Unknown command: ") + parts[0]);
+        }
+
+        // if parts[0] == "ECHO" {
+        //     println!("{:?}", parts[1]);
+        // } else if parts[0] == "PUSH" {
+        //     store.insert(parts[1]);
+        // } else if parts[0] == "VIEW" {
+        //     println!("{}", store.view(0));
+        // } else if parts[0] == "REMOVE" {
+        //     store.delete(parts[1]);
+        // } else {
+        //     println!("Invalid command. Options: ECHO PUSH VIEW REMOVE");
+        // }
+    }
+
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,27 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::Read;
+use std::io::Write;
 use std::net::TcpListener;
 
-use rstore::datum::Datum;
-
-fn parse_input(store: &mut Datum, input: String) {
-    let cmds: Vec<&str> = input.split("|").collect();
-    for cmd in cmds {
-        let parts: Vec<&str> = cmd.split(" ").collect();
-
-        if parts[0] == "ECHO" {
-            println!("{:?}", parts[1]);
-        } else if parts[0] == "PUSH" {
-            store.insert(parts[1]);
-        } else if parts[0] == "VIEW" {
-            println!("{}", store.view(0));
-        } else if parts[0] == "REMOVE" {
-            store.delete(parts[1]);
-        } else {
-            println!("Invalid command. Options: ECHO PUSH VIEW REMOVE");
-        }
-    }
-}
-
 fn main() -> io::Result<()> {
-    let mut store = Datum::new();
+    let mut store = HashMap::new();
 
     loop {
         let listener = TcpListener::bind("127.0.0.1:9876")?;
 
-        for stream in listener.incoming() {
+        for stream_result in listener.incoming() {
+            let mut stream = stream_result.unwrap();
             let mut buffer: [u8; 1024] = [0; 1024];
-            let size = stream.unwrap().read(&mut buffer).unwrap();
-            parse_input(
+            let size = stream.read(&mut buffer).unwrap();
+            let msg = rstore::parse_input(
                 &mut store,
                 String::from_utf8(buffer[0..size].to_vec()).unwrap().clone(),
             );
+
+            if let Some(message) = msg {
+                let _ = stream.write(&message.into_bytes());
+            }
         }
     }
 }
